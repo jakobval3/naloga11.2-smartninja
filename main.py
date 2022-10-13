ab511ce38f51a1c6681894dc760c4bf19fb4bcaa
tilenavs <tilenavs@gmail.com>
2022-10-13 09:23:02 +0200

    done

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ secret = random.randint(1, 30)
 print(secret)
 attempts = 0
 
-player= input("who is playing? ")
+player = input("who is playing? ")
 
 with open("score.json", "r") as file:
-    score_list=json.loads(file.read())
+    score_list = json.loads(file.read())
 
 for score_dict in score_list:
-    print(f"{score_dict['attempts']} attempts, by {score_dict['player']} on {score_dict['date']},wrong guesses were: {score_dict['wrong_guesses']}")
+    print(f"{score_dict['attempts']} attempts, by {score_dict['player']} on {score_dict['date']}, wrong guesses were: {score_dict['wrong_guesses']}")
+   #print(score_dict)
 
 
-wrong_guesses= []
+wrong_guesses = []
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
     attempts += 1
@@ -24,10 +25,10 @@ while True:
     if guess == secret:
         print("You've guessed it - congratulations! It's number " + str(secret))
         print("Attempts needed: " + str(attempts))
-        score_list.append({"attempts": attempts,"player": player, "date": str(datetime.datetime.now()), "wrong_guesses": wrong_guesses})
+        score_list.append({"attempts": attempts, "player": player, "date": str(datetime.datetime.now()), "wrong_guesses": wrong_guesses})
         with open( "score.json", "w") as file:
             file.write(json.dumps(score_list))
-            print(f"You guessed correctly, it's {str(secret)}, you needed {str(attempts)}.")
+            print(f"You guessed correctly, it's {str(secret)}, you needed {str(attempts)} attempts.")
         break
     elif guess > secret:
         print("Your guess is not correct... try something smaller")
